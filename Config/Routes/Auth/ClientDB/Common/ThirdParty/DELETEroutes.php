e6d582bfae45a8ed1389c0e998c9289f17416a5e
“polygoncoin <polygon.co.in@gmail.com>
2025-07-11 17:56:57 +0530

    Refactoring changes

--- Config/Routes/Auth/ClientDB/Common/ThirdParty/DELETEroutes.php
+++ Config/Routes/Auth/ClientDB/Common/ThirdParty/DELETEroutes.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * API Route config
+ * php version 8.3
+ *
+ * @category  API_Route_Config
+ * @package   Openswoole_Microservices
+ * @author    Ramesh N Jangid <polygon.co.in@gmail.com>
+ * @copyright 2025 Ramesh N Jangid
+ * @license   MIT https://opensource.org/license/mit
+ * @link      https://github.com/polygoncoin/Openswoole-Microservices
+ * @since     Class available since Release 1.0.0
+ */
 namespace Microservices\Config\Routes\Auth\ClientDB\Common\ThirdParty;
 
 return [
--- Config/Routes/Auth/ClientDB/Groups/Client001UserGroup2/PUTroutes.php
+++ Config/Routes/Auth/ClientDB/Groups/Client001UserGroup2/PUTroutes.php
@@ -1,15 +1,27 @@
 <?php
+/**
+ * API Route config
+ * php version 8.3
+ *
+ * @category  API_Route_Config
+ * @package   Openswoole_Microservices
+ * @author    Ramesh N Jangid <polygon.co.in@gmail.com>
+ * @copyright 2025 Ramesh N Jangid
+ * @license   MIT https://opensource.org/license/mit
+ * @link      https://github.com/polygoncoin/Openswoole-Microservices
+ * @since     Class available since Release 1.0.0
+ */
 namespace Microservices\Config\Routes\Auth\ClientDB\Groups\Client001UserGroup1;
 
 return array_merge (
-    include $Constants::$DOC_ROOT . 
+    require $Constants::$DOC_ROOT . 
         DIRECTORY_SEPARATOR . 'Config' . 
         DIRECTORY_SEPARATOR . 'Routes' . 
         DIRECTORY_SEPARATOR . 'Auth' . 
         DIRECTORY_SEPARATOR . 'ClientDB' . 
         DIRECTORY_SEPARATOR . 'Common' . 
         DIRECTORY_SEPARATOR . 'PUTroutes.php',
-    include $Constants::$DOC_ROOT . 
+    require $Constants::$DOC_ROOT . 
         DIRECTORY_SEPARATOR . 'Config' . 
         DIRECTORY_SEPARATOR . 'Routes' . 
         DIRECTORY_SEPARATOR . 'Auth' . 
