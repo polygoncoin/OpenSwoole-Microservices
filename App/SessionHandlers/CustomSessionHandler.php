224f6df3efba4b1a730999891a17b3e2bf4de787
“polygoncoin <polygon.co.in@gmail.com>
2025-11-14 22:40:08 +0530

    Refactoring

--- App/SessionHandlers/CustomSessionHandler.php
+++ App/SessionHandlers/CustomSessionHandler.php
@@ -379,14 +379,22 @@ class CustomSessionHandler implements
             isset($this->container->sessionOptions['read_and_close'])
             && $this->container->sessionOptions['read_and_close'] === true
         ) {
-            // Remove all Set-Cookie headers
-            // header_remove(name: 'Set-Cookie');
-            setcookie(
-                name: $this->sessionName,
-                value: '',
-                expires_or_options: 1,
-                path: $this->container->sessionOptions['cookie_path']
-            );
+            // Remove Session Set-Cookie headers
+            $headers = headers_list();
+            $headerFound = false;
+            foreach ($headers as $index => $header) {
+                if (strpos($header, $this->sessionName) !== false) {
+                    unset($headers[$index]);
+                    $headerFound = true;
+                    break;
+                }
+            }
+            if ($headerFound) {
+                header_remove();
+                foreach ($headers as &$header) {
+                    header(header: $header);
+                }
+            }
         }
     }
 }
