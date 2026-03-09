2c76e94b82b3ef5ed58db4e4c0a364a0e647608a
“polygoncoin <polygon.co.in@gmail.com>
2026-03-09 14:57:06 +0530

    Converted Indentation to Tab

--- Sql/client_master.sql
+++ Sql/client_master.sql
@@ -1,68 +1,71 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-	`id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	`client_id` int NOT NULL,
-	`firstname` varchar(255) NOT NULL,
-	`lastname` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`username` varchar(255) NOT NULL,
-	`password_hash` varchar(255) NOT NULL,
-	`allowed_cidr` text,
-	`group_id` int NOT NULL,
-	`token` varchar(255) DEFAULT NULL,
-	`token_ts` int UNSIGNED DEFAULT 0,
-	`rateLimitMaxRequest` int DEFAULT NULL,
-	`rateLimitMaxRequestWindow` int DEFAULT NULL,
-	`comments` varchar(255) DEFAULT NULL,
-	`created_by` int DEFAULT NULL,
-	`created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`approved_by` int DEFAULT NULL,
-	`approved_on` timestamp NULL DEFAULT NULL,
-	`updated_by` int DEFAULT NULL,
-	`updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	`is_approved` enum('Yes','No') NOT NULL DEFAULT 'No',
-	`is_disabled` enum('Yes','No') NOT NULL DEFAULT 'No',
-	`is_deleted` enum('Yes','No') NOT NULL DEFAULT 'No'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `client_id` INT NOT NULL,
+    `firstname` VARCHAR(255) NOT NULL,
+    `lastname` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `password_hash` VARCHAR(255) NOT NULL,
+    `allowed_cidr` TEXT,
+    `group_id` INT NOT NULL,
+    `token` VARCHAR(255) DEFAULT NULL,
+    `token_ts` INT UNSIGNED DEFAULT 0,
+    `rateLimitMaxRequest` INT DEFAULT NULL,
+    `rateLimitMaxRequestWindow` INT DEFAULT NULL,
+    `comments` VARCHAR(255) DEFAULT NULL,
+    `created_by` INT DEFAULT NULL,
+    `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `approved_by` INT DEFAULT NULL,
+    `approved_on` TIMESTAMP NULL DEFAULT NULL,
+    `updated_by` INT DEFAULT NULL,
+    `updated_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `is_approved` ENUM('Yes','No') NOT NULL DEFAULT 'No',
+    `is_disabled` ENUM('Yes','No') NOT NULL DEFAULT 'No',
+    `is_deleted` ENUM('Yes','No') NOT NULL DEFAULT 'No',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `address`;
 CREATE TABLE `address` (
-	`id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	`client_id` int NOT NULL,
-	`user_id` int NOT NULL DEFAULT 0,
-	`address` varchar(255) NOT NULL,
-	`created_by` int DEFAULT NULL,
-	`created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`approved_by` int DEFAULT NULL,
-	`approved_on` timestamp NULL DEFAULT NULL,
-	`updated_by` int DEFAULT NULL,
-	`updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	`is_approved` enum('Yes','No') NOT NULL DEFAULT 'No',
-	`is_disabled` enum('Yes','No') NOT NULL DEFAULT 'No',
-	`is_deleted` enum('Yes','No') NOT NULL DEFAULT 'No'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `client_id` INT NOT NULL,
+    `user_id` INT NOT NULL DEFAULT 0,
+    `address` VARCHAR(255) NOT NULL,
+    `created_by` INT DEFAULT NULL,
+    `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `approved_by` INT DEFAULT NULL,
+    `approved_on` TIMESTAMP NULL DEFAULT NULL,
+    `updated_by` INT DEFAULT NULL,
+    `updated_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `is_approved` ENUM('Yes','No') NOT NULL DEFAULT 'No',
+    `is_disabled` ENUM('Yes','No') NOT NULL DEFAULT 'No',
+    `is_deleted` ENUM('Yes','No') NOT NULL DEFAULT 'No',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
-	`id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	`parent_id` int NOT NULL DEFAULT 0,
-	`name` varchar(255) NOT NULL,
-	`created_by` int DEFAULT NULL,
-	`created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`approved_by` int DEFAULT NULL,
-	`approved_on` timestamp NULL DEFAULT NULL,
-	`updated_by` int DEFAULT NULL,
-	`updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	`is_approved` enum('Yes','No') NOT NULL DEFAULT 'No',
-	`is_disabled` enum('Yes','No') NOT NULL DEFAULT 'No',
-	`is_deleted` enum('Yes','No') NOT NULL DEFAULT 'No'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `parent_id` INT NOT NULL DEFAULT 0,
+    `name` VARCHAR(255) NOT NULL,
+    `created_by` INT DEFAULT NULL,
+    `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `approved_by` INT DEFAULT NULL,
+    `approved_on` TIMESTAMP NULL DEFAULT NULL,
+    `updated_by` INT DEFAULT NULL,
+    `updated_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `is_approved` ENUM('Yes','No') NOT NULL DEFAULT 'No',
+    `is_disabled` ENUM('Yes','No') NOT NULL DEFAULT 'No',
+    `is_deleted` ENUM('Yes','No') NOT NULL DEFAULT 'No',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS `api_cache`;
 CREATE TABLE `api_cache` (
-	`key` CHAR(128) NOT NULL,
-	`value` BLOB,
-	UNIQUE INDEX api_cache_key (`key`)
+    `key` CHAR(128) NOT NULL,
+    `value` BLOB,
+    UNIQUE INDEX api_cache_key (`key`)
 ) ENGINE=InnoDB;
 
 LOCK TABLES `user` WRITE;
